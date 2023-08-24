6d32a7485f11f72e529ffc0131be14322badff01
Yerong <yerong.li@outlook.com>
2023-08-24 01:09:11 -0500

    10000 samples

--- scripts/eval_flan-t5.sh
+++ scripts/eval_flan-t5.sh
@@ -34,7 +34,7 @@ CUDA_VISIBLE_DEVICES=0 python src/run_uie.py \
    --overwrite_output_dir \
    --overwrite_cache \
    --output_dir output \
-   --max_predict_samples 5000 \
+   --max_predict_samples 10000 \
 
 
 # CUDA_VISIBLE_DEVICES=0 deepspeed --master_port $port src/run_uie.py \
