ed7781ec26e02320b1c68ede55010d9259bac649
Yerong <yerong.li@outlook.com>
2023-08-26 01:32:03 -0500

    Print as dictionary

--- ana.py
+++ ana.py
@@ -4,9 +4,21 @@ import json
 with open('output/predict_eval_predictions.jsonl', 'r') as file:
     lines = file.readlines()
 
-# Extract and print all "Task" strings
+# Create a dictionary to store datasets for each task
+task_datasets = {}
+
+# Extract and store "Task" and "Dataset" pairs
 for line in lines:
     data = json.loads(line)
     task = data.get('Task')
-    if task:
-        print(task)
+    dataset = data.get('Dataset')
+    
+    if task and dataset:
+        if task in task_datasets:
+            task_datasets[task].add(dataset)
+        else:
+            task_datasets[task] = {dataset}
+
+# Print the "Task" and corresponding "Dataset" sets
+for task, datasets in task_datasets.items():
+    print(f'{{"Task": "{task}", "Dataset": {json.dumps(list(datasets))}}}')
