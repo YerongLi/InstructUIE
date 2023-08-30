d1431dfc9abdddf5063dbf2e2ae49a6b3a460946
Yerong <yerong.li@outlook.com>
2023-08-30 10:48:38 -0500

    Add schema field

--- fetch_instruction.py
+++ fetch_instruction.py
@@ -16,21 +16,26 @@ for line in lines:
 
     if "The output format" in instruction:
         instruction_before = instruction.split("The output format")[0].strip()
+        schema = instruction.split("The output format")[1].strip()
     elif "Output format" in instruction:
         instruction_before = instruction.split("Output format")[0].strip()
+        schema = instruction.split("Output format")[1].strip()
     else:
         instruction_before = None
+        schema = None
 
     if task and dataset:
         task_dataset = (task, dataset)
         if task_dataset in task_datasets_instructions:
-            task_datasets_instructions[task_dataset].add(instruction_before)
+            task_datasets_instructions[task_dataset].add((instruction_before, schema))
         else:
-            task_datasets_instructions[task_dataset] = {instruction_before}
+            task_datasets_instructions[task_dataset] = {(instruction_before, schema)}
 
 # Print the "Task," "Dataset," and corresponding unique "Instructions" sets
 for (task, dataset), instructions in task_datasets_instructions.items():
     print(f'Task: {task}')
     print(f'Dataset: {dataset}')
-    print(f'Instructions: {json.dumps(list(instructions))}')
+    for instruction, schema in instructions:
+        print(f'Instruction: {instruction}')
+        print(f'Schema: {schema}')
     print()
