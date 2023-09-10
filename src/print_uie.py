103d9c78b744ef1fa46ad0a4295dfb55e1fdc4e0
Yerong <yerong.li@outlook.com>
2023-09-10 01:00:17 -0500

    Dump predict_dataset to seed_task_ie.json

--- src/print_uie.py
+++ src/print_uie.py
@@ -470,7 +470,7 @@ def main():
         reformatted_entry = {
             "id": f"seed_task_{entry['Instance']['id']}",
             "name": entry["Task"],
-            "instruction": entry["Instance"]["instruction"],
+            "instruction": entry["Instance"]["instruction"].replace(' \nText: {0} \nAnswer:', '').strip(),
             "schema": entry["Instance"].get("instruction", ""),
             "input": entry["Instance"]["sentence"],
             "output": entry["Instance"]["label"],
