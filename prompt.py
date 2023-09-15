1bae9149513a460f2fa77627e4e8bd3cd90a01cd
Yerong <yerong.li@outlook.com>
2023-09-15 16:44:07 -0500

    Fill into prompt

---
+++ prompt.py
@@ -0,0 +1,30 @@
+import json
+import random
+```
+Fill instructions into a big prompt
+```
+def generate_instruction_prompts(config_file, num_prompts=8):
+    # Load instruction config from the JSON file
+    with open(config_file, 'r') as f:
+        instruction_config = json.load(f)
+
+    # Check if the "RE" key exists in the config
+    if "RE" in instruction_config:
+        # Randomly sample 'num_prompts' instructions from "RE"
+        sampled_instructions = random.sample(instruction_config["RE"], num_prompts)
+
+        # Create template prompts using the sampled instructions
+        prompts = []
+        for idx, instruction in enumerate(sampled_instructions, start=1):
+            prompt = f"{idx}. {instruction['instruction']}"
+            prompts.append(prompt)
+
+        return prompts
+    else:
+        return []
+
+# Example usage:
+if __name__ == "__main__":
+    instruction_prompts = generate_instruction_prompts("configs/instruction_config.json")
+    for idx, prompt in enumerate(instruction_prompts, start=1):
+        print(f"{idx}. {prompt}")
