c82dc73d8c51429f5a6210a0498b3211ff63348c
Yerong <yerong.li@outlook.com>
2023-09-15 17:22:20 -0500

    Fill into prompt

--- prompt.py
+++ prompt.py
@@ -6,10 +6,13 @@ def generate_instruction_prompts(config_file, num_prompts=8):
     with open(config_file, 'r') as f:
         instruction_config = json.load(f)
 
-    # Check if the "RE" key exists in the config
-    if "RE" in instruction_config:
-        # Randomly sample 'num_prompts' instructions from "RE"
-        sampled_instructions = random.sample(instruction_config["RE"], num_prompts)
+    # Initialize a dictionary to store prompts from all sections
+    all_prompts = {}
+
+    # Loop through each section in the instruction config
+    for section, instructions in instruction_config.items():
+        # Randomly sample 'num_prompts' instructions from the section
+        sampled_instructions = random.sample(instructions, num_prompts)
 
         # Create template prompts using the sampled instructions
         prompts = []
@@ -18,12 +21,19 @@ def generate_instruction_prompts(config_file, num_prompts=8):
             prompts.append(prompt)
 
         # Create the final header and combine prompts into a single string
-        header = "Come up with various instructions for information extraction tasks, Coreference Resolution, Relation Extraction, Aspect Extraction, Argument Mining"
+        header = f"Come up with various instructions for {section}"
         instruction_string = "\n".join(prompts)
 
-        return f"{header}\n{instruction_string}\n9."
-    else:
-        return ""
+        # Store the prompts for this section in the dictionary
+        all_prompts[section] = f"{header}\n{instruction_string}"
+
+    # Add a placeholder for the 9th instruction
+    all_prompts["9. Placeholder"] = ""
+
+    # Combine prompts from all sections into a single string
+    combined_prompts = "\n\n".join(all_prompts.values())
+
+    return combined_prompts
 
 # Example usage:
 if __name__ == "__main__":
