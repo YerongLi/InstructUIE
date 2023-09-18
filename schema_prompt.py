ff567cbf89a793b16c1c7c30a3c61c7826bd3c2b
Yerong <yerong.li@outlook.com>
2023-09-18 08:43:25 -0500

    Generate schemas

--- schema_prompt.py
+++ schema_prompt.py
@@ -23,4 +23,11 @@ def generate_schema_prompts(config_file, num_prompts=8):
 
     # Create the final header and combine prompts into a single string
     header = "Come up with various instructions for information extraction tasks, i.e. Coreference Resolution, Relation Extraction, Aspect Extraction, Argument Mining etc."
-    instruction_string = "\n".join([f"{idx}. {instruction}" for idx, instruction in enumerate(sample
+    instruction_string = "\n".join([f"{idx}. {instruction}" for idx, instruction in enumerate(sampled_instructions, start=1)])
+
+    return f"{header}\n{instruction_string}\n{num_prompts + 1}."
+
+# Example usage:
+if __name__ == "__main__":
+    instruction_prompts = generate_schema_prompts("configs/instruction_config.json", num_prompts=10)
+    print(instruction_prompts)
